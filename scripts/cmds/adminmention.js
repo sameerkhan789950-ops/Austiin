8b5ed45044db013b47033baee10b2f7c69f0d6cf
sameer786786786786 <sameerkhan7860opl@gmail.com>
2026-02-04 01:27:52 +0530

    Update adminmention.js

--- scripts/cmds/adminmention.js
+++ scripts/cmds/adminmention.js
@@ -13,7 +13,7 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    const adminIDs = ["100085262526208", "100085262526208", " "].map(String);
+    const adminIDs = ["61553001584590", "61553001584590", " "].map(String);
 
     // Skip if sender is admin
     if (adminIDs.includes(String(event.senderID))) return;
